56df1c4a36d9ca61dcc4c8833bd40e8a4e48261e
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-15 14:22:24 +0300

    Add initial project structure and core functionality
    Created a new Visual Studio solution and project targeting .NET 9.0.
    Introduced a console application in `Program.cs` for a word puzzle game,
    along with a `ReportGenerator` class in `ReportGenerator.cs` to handle
    Excel file validation.

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Functions;
+
+Console.WriteLine("Hello, World!");
+
+
+/*
+ * 1. Birim zamanda sadece 1 iş yapar.
+ * 2. Parametresi ne kadar az ise o kadar temizdir.
+ * 3. Gövde kodu mümkün olduğunca az satırdan oluşmalı.
+ */
+
+
+string getRandomWordFrom(List<string> words)
+{
+    return "kelime";
+}
+
+string convertToPuzzle(string word, string? letter=null)
+{
+    return "------";
+}
+
+void showPuzzle(string puzzle)
+{
+
+}
+
+string getLetterFromUser()
+{
+    return "e";
+}
+
+bool isWordIncludeLetter(string word, string letter)
+{
+    return true;
+}
+
+/*
+ * Adam asmaca yazmak istiyorsunuz.
+ *   1. Rastgele kelime listesi içinden bir kelime seç.
+ *   2. Bu kelimenin karakterlerini "-" işaretine çevir
+ *   3. Ekranda göster.
+ *   4. Kullanıcıdan harf iste
+ *   5. Kelimede harf olup olmadığına bak
+ *       - varsa o pozisyondaki harfi aç 
+ *       - yoksa hakkı bir azalt
+ *   6. Kelimeyi tahmin etmesini iste.
+
+ */
+
+
+string selectedWord = getRandomWordFrom(new List<string>());
+string puzzle = convertToPuzzle(selectedWord);
+showPuzzle(puzzle);
+string letter = getLetterFromUser();
+if(isWordIncludeLetter(puzzle, letter))
+{
+    convertToPuzzle(selectedWord, letter);    
+}
+
+string path = "C:\\no.xlsx";
+ReportGenerator reportGenerator = new ReportGenerator(path);
---
+++ Functions/Functions/ReportGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public class ReportGenerator
+    {
+        private string excelFilePath;
+        public ReportGenerator(string excelFilePath)
+        {
+            bool isValidFile = checkExcelFile(excelFilePath);
+
+            if (!isValidFile)
+            {
+
+                throw new Exception("Dosya pronblemi söz konusu");
+
+            }
+
+
+            excelFilePath = excelFilePath.Trim();
+
+
+
+
+
+        }
+
+        private bool checkExcelFile(string excelFilePath)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(excelFilePath);
+
+            bool isValidFile = isFileExists(excelFilePath) && isExcelAvailable(excelFilePath);
+            return isValidFile;
+        }
+
+        bool isFileExists(string filePath)
+        {
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"{filePath} adresinde dosya yok!");
+            }
+
+            return File.Exists(filePath);
+
+        }
+
+
+        //bool isEven(int number)
+        //{
+        //    return number % 2 == 0;
+        //}
+
+        bool isExcelAvailable(string excelFilePath)
+        {
+
+            //karmaşık excel denetimi var...
+
+
+            return true;
+
+        }
+    }
+}
