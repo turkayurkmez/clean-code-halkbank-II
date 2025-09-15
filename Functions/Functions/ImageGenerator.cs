4a2dce1ad84d66d170631c6212cf75688ff0f5d8
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-15 14:51:19 +0300

    Add ImageOptions and ImageGenerator classes
    Introduces a new namespace `Functions` with two classes:
    `ImageOptions` for defining image properties such as
    `Width`, `Height`, `IsColored`, and `Format`, and
    `ImageGenerator` with a `Generate` method that accepts
    an `ImageOptions` object. The method body is currently
    empty, setting the stage for future implementation.

---
+++ Functions/Functions/ImageGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+
+    public class ImageOptions
+    {
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool IsColored { get; set; }
+        public string Format { get; set; }
+
+    }
+    public class ImageGenerator
+    {
+        public void Generate(ImageOptions options)
+        {
+            
+        }
+    }
+}
