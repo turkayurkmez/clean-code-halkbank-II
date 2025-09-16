7169f3748376c2ee669f037f77ec9cc44af6846e
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-16 11:59:42 +0300

    Add SOLID principles projects and clean code examples
    - Updated `SOLID.sln` to include "DependencyInversion".
    - Created `DirtyCodeVSCleanCode.sln` with "DirtyCode" and "CleanCode" projects.
    - Added `CleanCode.csproj` and `DirtyCode.csproj` with SDK and framework settings.
    - Implemented console applications in `Program.cs` for both "CleanCode" and "DirtyCode" to manage time entries and billing.
    - Created `DependencyInversion.csproj` with a focus on the Dependency Inversion Principle.
    - Introduced a new `Program.cs` in "DependencyInversion" demonstrating interface usage for sending reports.
    - Added `ReportSender.cs` to define `ISender` interface and `ReportSender` class for better dependency management.

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInversion;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Büyük sistemler, küçüklere değil
+ * küçük nesneler, büyüklere bağımlı olsun.
+ * 
+ * Başka bir deyişle bağımlılık dışarıdan yönetilsin!
+ */
+
+MailSender mailSender = new MailSender();
+WhatsAppSender whatsAppSender = new WhatsAppSender();
+SlackSender slackSender = new SlackSender();
+
+
+ReportSender reportSender = new ReportSender(mailSender);
+ReportSender report2 = new ReportSender(slackSender);
+ReportSender report3 = new ReportSender(whatsAppSender);
+
+
+//reportSender.MailSender = mailSender;
+
+//MailSender mailSender2 = new MailSender();
+//reportSender.MailSender = mailSender2;,
+reportSender.Send();
+report2.Send();
+report3.Send();
+
---
+++ SOLID/DependencyInversion/ReportSender.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+
+    public interface ISender
+    {
+        void Send();
+    }
+    public class ReportSender
+    {
+
+        private readonly ISender sender;
+
+        public ReportSender(ISender sender)
+        {
+            this.sender = sender;    
+        }
+
+        //public MailSender MailSender { get; set; }
+        public void Send()
+        {
+            //mailSender instance'ını burada aldığım için; Büyük nesne (ReportSender) çok kullanılacak alt nesneye (MailSender) bağımlı...
+            //MailSender mailSender = new MailSender();
+
+            //İşte bağımlılığı gösteren satır:
+            sender.Send();
+        }
+    }
+
+    public class MailSender : ISender
+    {
+        public void Send() {
+            Console.WriteLine("Mail ile gönderildi");
+        }
+    }
+
+    public class WhatsAppSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Whatsapp ile gönderildi");
+        }
+    }
+
+    public class SlackSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Slack ...");
+        }
+    }
+}
